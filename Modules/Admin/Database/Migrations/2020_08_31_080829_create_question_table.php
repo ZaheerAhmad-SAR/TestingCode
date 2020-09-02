f47231f044be59fdd093064daacba136638b573f
Saqib-QA <sz@qapak.org>
2020-09-02 11:11:36 +0500

    form fields migration updated

--- Modules/Admin/Database/Migrations/2020_08_31_080829_create_question_table.php
+++ Modules/Admin/Database/Migrations/2020_08_31_080829_create_question_table.php
@@ -14,15 +14,18 @@ class CreateQuestionTable extends Migration
     public function up()
     {
         Schema::create('question', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary()->unique();
             $table->uuid('form_field_id');
+            $table->foreign('form_field_id')->references('id')->on('form_field')->onDelete('cascade')->onUpdate('cascade');
             $table->uuid('phase_steps_id');
+            $table->foreign('phase_steps_id')->references('id')->on('phase_steps')->onDelete('cascade')->onUpdate('cascade');
             $table->string('question_text')->nullable();
             $table->string('c_disk')->nullable();
             $table->string('measurement_unit')->nullable();
             $table->enum('is_dependent', array('no', 'yes'))->default('no');
             $table->text('dependent_on')->nullable();
             $table->text('annotations')->nullable();
+            $table->timestamp('deleted_at')->nullable();
             $table->timestamps();
         });
     }
