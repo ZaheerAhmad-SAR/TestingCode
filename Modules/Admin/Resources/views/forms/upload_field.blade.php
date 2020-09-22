4ee6aa640c358938c604a17980b49b2b4a540bcf
mjqapak <mj@qapak.org>
2020-09-22 15:46:46 +0500

    for submit

--- Modules/Admin/Resources/views/forms/upload_field.blade.php
+++ Modules/Admin/Resources/views/forms/upload_field.blade.php
@@ -1,5 +1,9 @@
+@php
+$field_name = 'field_' . $question->id;
+@endphp
 <div class="form-group">
-    <label class="">{{$question->question_text}}</label>
-    <input type="file" name="{{$question->variable_name}}" class="form-control-ocap bg-transparent">
-    <small class="form-text">{{$question->formFields->text_info}}</small>
+    <label class="">{{ $question->question_text }}</label>
+    <input type="file" name="{{ $field_name }}" onchange="submitFormField{{ $formNameStr }}('{{ $field_name }}');"
+        class="form-control-ocap bg-transparent">
+    <small class="form-text">{{ $question->formFields->text_info }}</small>
 </div>
