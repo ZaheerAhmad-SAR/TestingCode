cc50e97ca4c55c4252140aec25a660c5765b6eca
mjqapak <mj@qapak.org>
2020-10-08 12:21:34 +0500

    query popup

--- Modules/Admin/Resources/views/forms/form_fields/info_popup.blade.php
+++ Modules/Admin/Resources/views/forms/form_fields/info_popup.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex mt-3 mt-md-0 ml-auto">
 <span class="ml-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="cursor: pointer;"><i class="fas fa-eye" style="margin-top: 12px;" data-container="body" data-toggle="popover" data-trigger="hover" title="{{ $question }}" data-content="{{ $text_info }}"></i></span>
 </div>
-@push('last_scripts')
+@push('script_last')
 <script>
 $(function () {
   $('[data-toggle="popover"]').popover()
