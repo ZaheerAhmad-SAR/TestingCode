ba6be5ae6bd47353fa288c7745269bdd7de6eb79
mjqapak <mj@qapak.org>
2020-10-12 17:14:47 +0500

    info popup image issue fixed

--- Modules/Admin/Resources/views/forms/form_fields/textarea_field.blade.php
+++ Modules/Admin/Resources/views/forms/form_fields/textarea_field.blade.php
@@ -6,7 +6,7 @@
                 onchange="validateAndSubmitField('{{ $stepIdStr }}', '{{ $sectionIdStr }}', '{{ $question->id }}', '{{ $field_name }}', '{{ $fieldId }}');"
                 class="form-control-ocap bg-transparent">{{ $answer->answer }}</textarea>
         </div>
-        <div class="col-1">@include('admin::forms.form_fields.info_popup', ['question'=>$question->question_text, 'text_info'=>$question->formFields->text_info])</div><div class="col-1">@include('admin::forms.form_fields.query_popup')</div>
+        <div class="col-1">@include('admin::forms.form_fields.info_popup', ['question'=>$question])</div><div class="col-1">@include('admin::forms.form_fields.query_popup')</div>
     </div>
 
 </div>
