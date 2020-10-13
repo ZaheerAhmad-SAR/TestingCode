6c94bedf8ddf8bb54b40edefa2c1e99b6e612ba1
Amir Khan <ak@qapak.org>
2020-10-13 12:21:06 +0500

    Amir commit

---
+++ Modules/UserRoles/Resources/views/users/grading-list.blade.php
@@ -0,0 +1,111 @@
+@extends ('layouts.home')
+
+@section('title')
+    <title> Grading List | {{ config('app.name', 'Laravel') }}</title>
+@stop
+
+@section('styles')
+
+    <style type="text/css">
+        /*.table{table-layout: fixed;}*/
+
+        .select2-container--default
+        .select2-selection--single {
+            background-color: #fff;
+             border: transparent !important;
+            border-radius: 4px;
+        }
+        .select2-selection__rendered {
+            font-weight: 400;
+            line-height: 1.5;
+            color: #495057 !important;
+            background-color: #fff;
+            background-clip: padding-box;
+            border: 1px solid #ced4da;
+            border-radius: .25rem;
+            transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        }
+    </style>
+    <!-- date range picker -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+    <!-- select2 -->
+    <link rel="stylesheet" href="{{ asset('public/dist/vendors/select2/css/select2.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('public/dist/vendors/select2/css/select2-bootstrap.min.css') }}"/>
+@endsection
+
+@section('content')
+
+    <div class="container-fluid site-width">
+        <!-- START: Breadcrumbs-->
+        <div class="row ">
+            <div class="col-12  align-self-center">
+                <div class="sub-header mt-3 py-3 align-self-center d-sm-flex w-100 rounded">
+                    <div class="w-sm-100 mr-auto"><h4 class="mb-0">Grading List</h4></div>
+                    <ol class="breadcrumb bg-transparent align-self-center m-0 p-0">
+                        <li class="breadcrumb-item">Dashboard</li>
+                        <li class="breadcrumb-item">Grading List</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <!-- END: Breadcrumbs-->
+
+        <!-- START: Card Data-->
+        <div class="row">
+            <div class="col-12 col-sm-12 mt-3">
+                <div class="card">
+
+                  
+                    <div class="card-body">
+                        <div class="table-responsive">
+
+                            <table class="table table-bordered" id="laravel_crud">
+                                <thead>
+                                    <tr>
+                                        <th>Study ID</th>
+                                        <th>Phase</th>
+                                        <th>Enrollement Date</th>
+                                        <th>Site Name</th>
+                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                   
+                                    <tr>
+                                   
+                                        <td colspan="4" style="text-align: center;"> No record found.</td>
+                                    </tr>
+                                    
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- Card ends -->
+            </div>
+        </div>
+        <!-- END: Card DATA-->
+    </div>
+
+@endsection
+@section('script')
+
+<!-- date range picker -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
+<!-- select2 -->
+<script src="{{ asset('public/dist/vendors/select2/js/select2.full.min.js') }}"></script>
+<script src="{{ asset('public/dist/js/select2.script.js') }}"></script>
+
+<script type="text/javascript">
+
+
+</script>
+@endsection
+
+
+
+
