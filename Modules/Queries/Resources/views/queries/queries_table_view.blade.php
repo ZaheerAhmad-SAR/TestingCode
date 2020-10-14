262de8800eead3b3a485e1752f2a7bc12c504f3b
amjad <ag@qapk.org>
2020-10-14 16:50:23 +0500

    Queries table is updated for more columns

---
+++ Modules/Queries/Resources/views/queries/queries_table_view.blade.php
@@ -0,0 +1,28 @@
+@php $count = 1; @endphp
+@foreach($records as $record)
+@php
+
+$queryUsersIds = Modules\Queries\Entities\QueryUser::where('query_id',$record->id)->pluck('user_id')->toArray();
+
+$queryUsersNames = App\User::whereIn('id',$queryUsersIds)->pluck('name')->toArray();
+$querySubmitedBy = App\User::find($record->queried_remarked_by_id);
+$queryUsersString = implode(',',$queryUsersNames);
+
+ @endphp
+<tr>
+    <td data-id="{{$record->id}}">{{$count++}}</td>
+    <td>{{$record->query_subject}}</td>
+    <td>{{$querySubmitedBy->name}}</td>
+    <td>{{$queryUsersString}}</td>
+    <td>{{date_format($record->created_at,'jS F Y h:i:s A')}}</td>
+    <td>{{$record->query_status}}</td>
+    <td class="replyModal" data-id="{{$record->id}}"><span style="cursor: pointer;"><i class="fab fa-rocketchat"></i></span></td>
+</tr>
+@endforeach
+
+@section('script')
+    <script type="text/javascript">
+
+    </script>
+@endsection
+
