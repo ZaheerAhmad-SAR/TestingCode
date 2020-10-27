5be9dad4cf69386528b849797f9a1dba8da1ffd5
Amir Khan <ak@qapak.org>
2020-10-27 12:08:16 +0500

    Amir commit

--- Modules/Queries/Database/Migrations/2020_10_14_063845_add_extra_column_to_queries_table.php
+++ Modules/Queries/Database/Migrations/2020_10_14_063845_add_extra_column_to_queries_table.php
@@ -14,9 +14,9 @@ class AddExtraColumnToQueriesTable extends Migration
     public function up()
     {
         Schema::table('queries', function (Blueprint $table) {
-        $table->text('query_subject')->after('messages')->nullable();
-        $table->text('query_url')->after('query_subject')->nullable();
-        $table->enum('query_type',array('user','role'))->after('query_url')->nullable();
+        // $table->text('query_subject')->after('messages')->nullable();
+        // $table->text('query_url')->after('query_subject')->nullable();
+        // $table->enum('query_type',array('user','role'))->after('query_url')->nullable();
         //$table->enum('query_status',array('new','open', 'close'))->after('module_name')->default('new')->change();
         });
     }
@@ -29,7 +29,7 @@ class AddExtraColumnToQueriesTable extends Migration
     public function down()
     {
         Schema::table('queries', function (Blueprint $table) {
-            $table->dropColumn(['query_subject','query_url','query_type']);
+            // $table->dropColumn(['query_subject','query_url','query_type']);
 
         });
     }
