ad1ac49af8133528be56b6c9fc3d669fa47dc036
mjqapak <mj@qapak.org>
2020-11-05 11:00:58 +0500

    preferences fixes

---
+++ Modules/Admin/Scopes/PreferencesByStudy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Admin\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class PreferencesByStudy implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where('study_id', 'like', session('current_study'));
+    }
+}
--- Modules/UserRoles/Database/Seeders/PreferencesTableSeeder.php
+++ Modules/UserRoles/Database/Seeders/PreferencesTableSeeder.php
@@ -24,8 +24,8 @@ class PreferencesTableSeeder extends Seeder
             Preference::create([
                 'id' => 1,
                 'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
-                'preference_title'    => 'Visit Activation',
-                'preference_value'    => 'Transmission',
+                'preference_title'    => 'VISIT_ACTIVATION',
+                'preference_value'    => 'Manual',
                 'is_selectable'       => 'yes', //yes/no
                 'preference_options'  => 'Transmission|Manual', //Pipe sign seperated options
                 'created_at'        => Carbon::now(),
@@ -34,7 +34,7 @@ class PreferencesTableSeeder extends Seeder
         } else {
             Preference::updateOrCreate(['id' => 1], [
                 'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
-                'preference_title'    => 'Visit Activation',
+                'preference_title'    => 'VISIT_ACTIVATION',
                 'preference_value'    => 'Transmission',
                 'is_selectable'       => 'yes', //yes/no
                 'preference_options'  => 'Transmission|Manual', //Pipe sign seperated options
@@ -45,38 +45,64 @@ class PreferencesTableSeeder extends Seeder
             Preference::create([
                 'id' => 2,
                 'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
-                'preference_title'    => 'Study Email',
+                'preference_title'    => 'STUDY_EMAIL',
                 'preference_value'    => 'study_email@study.com',
                 'is_selectable'       => 'no', //yes/no
                 'preference_options'  => '', //Pipe sign seperated options
                 'created_at'        => Carbon::now(),
                 'updated_at'        => Carbon::now()
             ]);
+        } else {
+            Preference::updateOrCreate(['id' => 2], [
+                'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
+                'preference_title'    => 'STUDY_EMAIL',
+                'preference_value'    => 'study_email@study.com',
+                'is_selectable'       => 'no', //yes/no
+                'preference_options'  => '', //Pipe sign seperated options
+            ]);
         }
+
         $preference = Preference::find(3);
         if (null === $preference) {
             Preference::create([
                 'id' => 3,
                 'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
-                'preference_title'    => 'Study Cc Email(s)',
+                'preference_title'    => 'STUDY_CC_EMAILS',
                 'preference_value'    => 'studyEmail1@study.com,studyEmail2@study.com,studyEmail3@study.com',
                 'is_selectable'       => 'no', //yes/no
                 'preference_options'  => '', //Pipe sign seperated options
                 'created_at'        => Carbon::now(),
                 'updated_at'        => Carbon::now()
             ]);
+        } else {
+            Preference::updateOrCreate(['id' => 3], [
+                'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
+                'preference_title'    => 'STUDY_CC_EMAILS',
+                'preference_value'    => 'studyEmail1@study.com,studyEmail2@study.com,studyEmail3@study.com',
+                'is_selectable'       => 'no', //yes/no
+                'preference_options'  => '', //Pipe sign seperated options
+            ]);
         }
+
         $preference = Preference::find(4);
         if (null === $preference) {
             Preference::create([
                 'id' => 4,
                 'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
-                'preference_title'    => 'Pagination per page',
+                'preference_title'    => 'PER_PAGE_PAGINATION',
                 'preference_value'    => '25',
                 'is_selectable'       => 'yes', //yes/no
                 'preference_options'  => '15|25|50|100|200|500|1000|5000', //Pipe sign seperated options
                 'created_at'        => Carbon::now(),
                 'updated_at'        => Carbon::now()
+            ]);
+        } else {
+            Preference::updateOrCreate(['id' => 4], [
+                'study_id' => 'ced232fb-2130-4edd-ba49-99ab8aa141e4',
+                'preference_title'    => 'PER_PAGE_PAGINATION',
+                'preference_value'    => '25',
+                'is_selectable'       => 'yes', //yes/no
+                'preference_options'  => '15|25|50|100|200|500|1000|5000', //Pipe sign seperated options
             ]);
         }
         /*********************************/
