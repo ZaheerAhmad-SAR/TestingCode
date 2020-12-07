d487b9890c72d16ccfdd9bca020aeb2a2ee9f7f4
mjqapak <mj@qapak.org>
2020-12-07 14:09:08 +0500

    data export

--- Modules/FormSubmission/Resources/views/exports/export_view.blade.php
+++ Modules/FormSubmission/Resources/views/exports/export_view.blade.php
@@ -1,6 +1,24 @@
 <table>
     <thead>
-        {!! $header !!}
+        <tr>
+            @foreach ($header as $headerCol)
+            <th>{!! $headerCol !!}</th>
+            @endforeach
+        </tr>
     </thead>
-    <tbody>{!! $body !!}</tbody>
+    <tbody>
+        @foreach ($body as $bodyCol)
+        <tr>
+            @foreach ($header as $headerColKey => $headerColVal)
+            @php
+            $val = '';
+            if(isset($bodyCol[$headerColKey])){
+                $val = $bodyCol[$headerColKey];
+            }
+            @endphp
+            <td>{!! $val !!}</td>
+            @endforeach
+        </tr>
+        @endforeach
+    </tbody>
 </table>
