e34b71e61a197f67db5008695531badcf6c3e91c
devamjad <ag@qapak.org>
2020-12-15 15:19:57 +0500

    assign sites & remove assign sites is completed

--- Modules/Admin/Entities/StudySite.php
+++ Modules/Admin/Entities/StudySite.php
@@ -27,25 +27,17 @@ class StudySite extends Model
 
     public static function checkAssignedStudySite($study_id, $site_id)
     {
-        $checkAssignedModilities = self::where('study_id', $study_id)
+        $checkAssignedSite = self::where('study_id', $study_id)
             ->where('site_id', $site_id)
             ->first();
-
         // check if this modality is already assigned to study
-        if ($checkAssignedModilities != null) {
-
+        if ($checkAssignedSite != null)
+        {
             return '<span class="badge badge-success">Yes</span>';
-
-        } else {
-
+        }
+        else
+        {
             return '<span class="badge badge-primary">No</span>';
-
         } // check ends
     }
-
-
 }
-
-
-
-
