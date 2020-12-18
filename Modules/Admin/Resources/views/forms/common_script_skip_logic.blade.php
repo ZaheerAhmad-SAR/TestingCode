6c7af5844d15b032ac364112ef965a1e5a29b754
abidullah667 <abidkhattak667@gmail.com>
2020-12-18 12:32:14 +0500

    change skip logic

--- Modules/Admin/Resources/views/forms/common_script_skip_logic.blade.php
+++ Modules/Admin/Resources/views/forms/common_script_skip_logic.blade.php
@@ -1,7 +1,6 @@
 @push('script')
    <script type="text/javascript">
    	function disabled_opposite(stepid,class_opposite_checkbox,index,current_class){
-
    		if($('.'+current_class+stepid+'_'+index).is(':checked')){
    			$('.'+class_opposite_checkbox+stepid+'_'+index).attr('disabled',true);
    		}else{
