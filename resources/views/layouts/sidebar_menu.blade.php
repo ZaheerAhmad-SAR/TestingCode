5a0fbb8a4811c62a6eac8d5833c80325dbf6bfda
Zaheer Ahmad <za@qapak.org>
2020-12-24 15:50:09 +0500

    latest

--- resources/views/layouts/sidebar_menu.blade.php
+++ resources/views/layouts/sidebar_menu.blade.php
@@ -498,57 +498,8 @@
           
         </ul>
         <!-- END: Menu-->
-        <ul style="list-style:none; padding-left: 0px; margin-left: 10px;" class="myDIV">
-             <li >
-                <button type="button" class="btn btn-primary btn-floating ">
-  <i class="fas fa-question" > &nbsp Support</i>
-</button>
-            </li>
-        </ul>
-<div class="hide" style="background-color: #000000; height: 400px; margin-left: 100px; margin-bottom: 500px; vertical-align:top;"><ul >
-                                    <li class="">
-                                        <a href="http://localhost/ocap_new/users">
-                                            System Users
-                                        </a>
-                                    </li>
-                                     <li class="">
-                                        <a href="http://localhost/ocap_new/roles">
-                                            <i class="fal fa-user-tag"></i>
-                                            Roles
-                                        </a>
-                                    </li>
-                                    <li class="">
-                                        <a href="http://localhost/ocap_new/sites">
-                                            <i class="fal fa-location-arrow"></i>
-                                            Sites
-                                        </a>
-                                    </li>
-                                    <li class="">
-                                        <a href="http://localhost/ocap_new/devices">
-                                            <i class="fal fa-calculator"></i>
-                                            Devices
-                                        </a>
-                                    </li>
-                                 <li class=" ">
-                                        <a href="http://localhost/ocap_new/modalities">
-                                            <i class="fal fa-mobile"></i>
-                                            Modalities
-                                        </a>
-                                    </li>
-                                
-                            </ul></div>
+    
+
     </div>
 
 </div>
-<style>
-.hide {
-  display: none;
-}
-    
-.myDIV:hover + .hide {
-  display: block;
-  color: red;
-  margin-left: 70px;
-  padding-bottom: 70px;
-}
-</style>
