163fab481aababfad24ac7b4bef61e93d011ee8c
Zaheer Ahmad <za@qapak.org>
2021-01-12 15:41:32 +0500

    checking git key

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -62,6 +62,7 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         if (config('app.env') == 'local') {
+            dd('i am here');
 
             return parent::render($request, $exception);
         }
