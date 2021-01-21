c6f3eb22df15ea8d3f07f157142359ae9ee57c00
devamjad <ag@qapak.org>
2021-01-21 11:52:55 +0500

    bug reporting code is updated to show a latest record on the time and also add a carbon library for datetime

--- resources/views/layouts/sidebar_menu.blade.php
+++ resources/views/layouts/sidebar_menu.blade.php
@@ -509,7 +509,7 @@
                 </li>
                 @endif
 
-               
+
 
             @endif
 
@@ -520,7 +520,7 @@
      <button type="button" class="btn btn-primary dropdown-toggle position-fixed" data-toggle="dropdown">  <i class="icon-question"></i> Support</button>
         <div class="dropdown-menu">
             @if(hasPermission(auth()->user(),'bug-reporting.create'))
-            <a href="#" class="dropdown-item"  data-toggle="modal" data-target="#reportabugmodel"><i class="fa fa-plus"></i>   Report a Bug</a>
+            <a href="javascript:void(0);" class="dropdown-item"  data-toggle="modal" data-target="#reportabugmodel"><i class="fa fa-plus"></i>   Report a Bug</a>
             <div class="dropdown-divider"></div>
             @endif
             <a href="#" class="dropdown-item"><i class="fa fa-plus"></i>  User Manual</a>
