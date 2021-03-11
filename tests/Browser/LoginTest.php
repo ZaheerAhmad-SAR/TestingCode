7464ccaf1b12d7a3a3993cd087f39ccef42be479
Amir Khan <ak@qapak.org>
2021-03-11 17:47:02 +0500

    amir commits

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -10,13 +10,18 @@ use Illuminate\Database\Seeder;
 
 class LoginTest extends DuskTestCase
 {
+    /* for running migration and seeding only once for all test cases in this class */
+    protected static $migrationRun = false;
 
-   public function setUp(): void
-    {
-        $this->appDB = env('DB_DATABASE');
+    public function setUp(): void {
         parent::setUp();
-        //$this->artisan('migrate:fresh');
-        $this->artisan('module:seed');
+
+        if(!static::$migrationRun) {
+            $this->appDB = env('DB_DATABASE');
+            //$this->artisan('migrate:fresh');
+            $this->artisan('module:seed');
+            static::$migrationRun = true;
+        }
     }
 
     /* @test */
@@ -31,6 +36,43 @@ class LoginTest extends DuskTestCase
                     ->press('Sign In')
                     ->assertSee('Please add study admin role first')
                     ->logout();
+        });
+    }
+
+    /* @test */
+    public function test_I_can_login_with_wrong_credientials_successfully()
+    {
+        // this test the login functionality with wrong credientials
+        $this->browse(function ($browser) {
+            $browser->visit('/login')
+                    ->type('email',  'ak@qapak.org')
+                    ->type('password', 'wrong_password')
+                    ->press('Sign In')
+                    ->assertSee('These credentials do not match our records.');
+        });
+    }
+
+    /* @test */
+    public function test_I_can_forget_password_successfully()
+    {
+        // this test the forget password functionality
+        $this->browse(function ($browser) {
+            $browser->visit('/password/reset')
+                    ->type('email',  'ak@qapak.org')
+                    ->press('Send Link')
+                    ->assertSee("We have e-mailed your password reset link!");
+        });
+    }
+
+    /* @test */
+    public function test_I_can_forget_password_with_wrong_email_successfully()
+    {
+        // this test the forget password functionality with wrong email credientials
+        $this->browse(function ($browser) {
+            $browser->visit('/password/reset')
+                    ->type('email',  'test@qapak.org')
+                    ->press('Send Link')
+                    ->assertSee('We can\'t find a user with that e-mail address.');
         });
     }
 
